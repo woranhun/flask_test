5d8cca3176250e3bb2e6a2b33a6722a283a461c4
Bence Orosz <bencus1226@gmail.com>
2018-05-17 20:33:32 +0200

    second video 9:23

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+  {
+    'author': 'Bence Orosz',
+    'title': 'Blog Post 1',
+    'content': 'First post content',
+    'date_posted': '2018.05.17.'
+  },
+  {
+    'author': 'Ervinbá',
+    'title': 'Blog Post 2',
+    'content': 'Second post content',
+    'date_posted': '2018.05.21.'
+  }
+
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-  return "<h1>Home page</h1>"
+  return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-  return"<h1>About page</h1>"
+  return render_template('about.html')
 
 
 if __name__ == '__main__':
